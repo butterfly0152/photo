d3a7b381b36a1798cfcd0c8296c9101ba6428245
butterfly0152 <butterfly0152@yahoo.com>
2016-04-13 11:17:51 +0300

    Added files via upload

---
+++ index2.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link rel ="stylesheet" type = "text/css" href = "stil.css" />
+    
+<title>Marius Pilaf</title>
+
+</head>
+
+<body> 
+
+<div class = "container-fluid">
+		
+	<div class = "col-md-6">
+        
+        <img class = "img-responsive" src = "./photo/fundalt.png" alt = "bride" />
+	
+    </div>   <!-- end of col-md-4 class -->       
+    
+    <div class = "middle">
+    	 <div class = "row">
+         	<div class = "col-md-3">
+    			<ul class = "list-unstyled">
+                    <li><a href = "#">Home</a></li> 
+                    <li><a href = "#">Wedding</a></li>
+                    <li><a href = "#">Session</a></li>
+                    <li><a href = "#">Kids</a></li>
+                    <li><a href = "#">Commercial</a></li>
+   		 		</ul>
+			</div>
+         </div> <!-- end of row class --> 
+	</div> <!-- end of middle class --> 
+
+</div> <!-- end of container-fluid class --> 
+
+   
+</body>
+</html>
